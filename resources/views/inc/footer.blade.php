66ad42ecadf1b7c88007c045b3f7824479976394
mtv90 <M_TV@gmx.de>
2019-01-20 13:04:43 +0100

    changes

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -4,5 +4,11 @@
             <li><a href="{{ url('/impressum') }}">Impressum</a></li>
             <li><a href="{{ url('/datenschutz') }}">Datenschutz</a></li>
         </ul>
-        <p>&copy; 2018 <b>corticalo</b></p>
+        <p>&copy; 2018 <b>corticalo</b></p> <hr>
+        <div class="row">
+            <small class="mr-2">Folge uns: </small>
+            <small class="mr-1"><a title="Facebook" href=""><i class="fa fa-facebook-square socialmedia"></i></a></small>
+            <small class="mr-1"><a title="Linkedin" href=""><i class="fa fa-linkedin-square socialmedia"></i></a></small>
+            <small class="mr-1"><a title="Xing" href=""><i class="fa fa-xing-square socialmedia"></i></a></small>
+        </div>
 </footer>
