4418a88de4b76d6eca99797a15a1cb420ec26f35
mtv90 <M_TV@gmx.de>
2019-01-20 03:15:05 +0100

    change seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -68,6 +68,123 @@ class DatabaseSeeder extends Seeder
             'right_id' => 1,
             'role_id' => 1
         ]);
+
+        DB::table('rights')->insert([
+            'rightname' => 'studyrights',
+            'studindex' => 1,
+            'studishow' => 1,
+            'studicreate' => 1,
+            'studiedit' => 1,
+            'studidelete' => 1,
+            'crfindex' => 1,
+            'crfshow' => 1,
+            'crfcreate' => 1,
+            'crfedit' => 1,
+            'crfdelete' => 1,
+            'formindex' => 1,
+            'formshow' => 1,
+            'formcreate' => 1,
+            'formedit' => 1,
+            'formdelete' => 1,
+            'choiceindex' => 1,
+            'choiceshow' => 1,
+            'choicecreate' => 1,
+            'choiceedit' => 1,
+            'choicedelete' => 1,
+            'resultindex' => 0,
+            'resultshow' => 0,
+            'resultcreate' => 0,
+            'resultedit' => 0,
+            'resultdelete' => 0,
+            'patindex' => 0,
+            'patshow' => 0,
+            'patcreate' => 0,
+            'patedit' => 0,
+            'patdelete' => 0,
+            'userindex' => 1,
+            'usershow' => 1,
+            'usercreate' => 0,
+            'useredit' => 1,
+            'userdelete' => 0,
+            'roleindex' => 0,
+            'roleshow' => 0,
+            'rolecreate' => 0,
+            'roleedit' => 0,
+            'roledelete' => 0,
+            'rightindex' => 0,
+            'rightshow' => 0,
+            'rightcreate' => 0,
+            'rightedit' => 0,
+            'rightdelete' => 0,
+            'stats' => 1,
+            
+        ]);
+        DB::table('roles')->insert([
+            'roletype' => 'Studienleiter'
+        ]);
+        DB::table('right_role')->insert([
+            'right_id' => 2,
+            'role_id' => 2
+        ]);
+        
+        DB::table('rights')->insert([
+            'rightname' => 'doctorrights',
+            'studindex' => 0,
+            'studishow' => 0,
+            'studicreate' => 0,
+            'studiedit' => 0,
+            'studidelete' => 0,
+            'crfindex' => 0,
+            'crfshow' => 0,
+            'crfcreate' => 0,
+            'crfedit' => 0,
+            'crfdelete' => 0,
+            'formindex' => 0,
+            'formshow' => 0,
+            'formcreate' => 0,
+            'formedit' => 0,
+            'formdelete' => 0,
+            'choiceindex' => 0,
+            'choiceshow' => 0,
+            'choicecreate' => 0,
+            'choiceedit' => 0,
+            'choicedelete' => 0,
+            'resultindex' => 1,
+            'resultshow' => 1,
+            'resultcreate' => 1,
+            'resultedit' => 1,
+            'resultdelete' => 1,
+            'patindex' => 1,
+            'patshow' => 1,
+            'patcreate' => 1,
+            'patedit' => 1,
+            'patdelete' => 1,
+            'userindex' => 1,
+            'usershow' => 1,
+            'usercreate' => 0,
+            'useredit' => 1,
+            'userdelete' => 0,
+            'roleindex' => 0,
+            'roleshow' => 0,
+            'rolecreate' => 0,
+            'roleedit' => 0,
+            'roledelete' => 0,
+            'rightindex' => 0,
+            'rightshow' => 0,
+            'rightcreate' => 0,
+            'rightedit' => 0,
+            'rightdelete' => 0,
+            'stats' => 0,
+            
+        ]);
+        DB::table('roles')->insert([
+            'roletype' => 'Arzt'
+        ]);
+        DB::table('right_role')->insert([
+            'right_id' => 3,
+            'role_id' => 3
+        ]);
+
         DB::table('formats')->insert([
             'formats' => 'Textfeld'
             ]);
