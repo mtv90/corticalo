f242ef811f696281584713534078e59b1a4f8181
mtv90 <M_TV@gmx.de>
2019-01-24 12:34:37 +0100

    de

--- resources/views/getstudy.blade.php
+++ resources/views/getstudy.blade.php
@@ -3,6 +3,13 @@
     <h5>{{ $study->studyname }}</h5> <small>von {{$study->director}} </small>
     <div class="col-md-12 alert border">
         <p>CRFs:</p> 
+        <div class="col-md-4">Antworten
+            @if(count($study->results) > 0) 
+                <span class="badge border" style="color:white; background-color:MediumSeaGreen;">{{count($study->results)}}</span>
+            @else
+                <span class="badge border" style="color:white; background-color:Tomato;">{{count($study->results)}}</span>
+            @endif 
+        </div>
         @foreach($study->crfs as $crf)
             <p><strong>{{$crf->crfName}}</strong></p>
             <div class="col-md-12">Patienten
@@ -10,13 +17,6 @@
                     <span class="badge border" style="color:white; background-color:MediumSeaGreen;">{{count($study->patients)}}</span>
                 @else
                     <span class="badge border" style="color:white; background-color:Tomato;">{{count($study->patients)}}</span>
-                @endif 
-            </div>
-            <div class="col-md-4">Antworten
-                @if(count($study->results) > 0) 
-                    <span class="badge border" style="color:white; background-color:MediumSeaGreen;">{{count($study->results)}}</span>
-                @else
-                    <span class="badge border" style="color:white; background-color:Tomato;">{{count($study->results)}}</span>
                 @endif 
             </div>
             <hr>
