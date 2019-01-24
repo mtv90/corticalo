310ae5e03d125f3c0a4affc8bb0a6640184b20d1
mtv90 <M_TV@gmx.de>
2019-01-24 13:04:06 +0100

    de

--- resources/views/getstudy.blade.php
+++ resources/views/getstudy.blade.php
@@ -2,7 +2,7 @@
 <div class="col-md-12">
     <h5>{{ $study->studyname }}</h5> <small>von {{$study->director}} </small>
     <div class="col-md-12 alert border">
-        <p>CRFs:</p> 
+        
         <div class="col-md-4">Antworten
             @if(count($study->results) > 0) 
                 <span class="badge border" style="color:white; background-color:MediumSeaGreen;">{{count($study->results)}}</span>
@@ -10,16 +10,17 @@
                 <span class="badge border" style="color:white; background-color:Tomato;">{{count($study->results)}}</span>
             @endif 
         </div>
+        <div class="col-md-12">Patienten       
+            @if(count($study->patients)>0)     
+                <span class="badge border" style="color:white; background-color:MediumSeaGreen;">{{count($study->patients)}}</span>
+            @else
+                <span class="badge border" style="color:white; background-color:Tomato;">{{count($study->patients)}}</span>
+            @endif 
+        </div>
+        <hr>
+        <p>vorhandene CRFs:</p> 
         @foreach($study->crfs as $crf)
             <p><strong>{{$crf->crfName}}</strong></p>
-            <div class="col-md-12">Patienten
-                @if(count($study->patients)>0) 
-                    <span class="badge border" style="color:white; background-color:MediumSeaGreen;">{{count($study->patients)}}</span>
-                @else
-                    <span class="badge border" style="color:white; background-color:Tomato;">{{count($study->patients)}}</span>
-                @endif 
-            </div>
-            <hr>
         @endforeach
     </div>
 </div>    
